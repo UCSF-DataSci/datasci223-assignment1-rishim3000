0c02269641452ac7df413ff061b0083573fed293
rishim3000 <rishivm@gmail.com>
2025-04-02 17:48:48 -0700

    updated w/ regex

--- email_hasher.py
+++ email_hasher.py
@@ -1,19 +1,6 @@
-#!/usr/bin/env python3
-"""
-Email Hasher Script
-
-This script takes an email address as a command line argument,
-hashes it using the SHA-256 algorithm, and writes the hash to a file.
-
-Usage:
-    python email_hasher.py <email_address>
-
-Example:
-    python email_hasher.py example@email.com
-"""
-
 import sys
 import hashlib
+import re
 
 def hash_email(email):
     hash_object = hashlib.sha256()
@@ -25,6 +12,10 @@ def write_hash_to_file(hash_value, filename="hash.email"):
     with open(filename, "w") as file:
         file.write(hash_value)
 
+def is_valid_email(email):
+    #checking for valid email address
+    email_regex = r"^[^@]+@[^@]+\.[a-zA-Z]{2,}$"
+    return re.match(email_regex, email)       
 
 def main():
     if len(sys.argv) != 2:
@@ -32,12 +23,11 @@ def main():
         sys.exit(1)
 
     email = sys.argv[1] #email from command line argument
-    if "@" not in email:
+    
+    if not is_valid_email(email):
         print("Invalid email address!")
         sys.exit(1)
-    if ".com" not in email:
-        print("Invalid email address!")
-        sys.exit(1)    
+
     hash_value = hash_email(email) #hash value of email input
     write_hash_to_file(hash_value) #write hash value to file
 
